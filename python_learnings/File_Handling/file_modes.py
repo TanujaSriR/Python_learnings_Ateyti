68644a44fd2310b8daa148dc8c4598a112f98151
TanujaSriR <tanuja.ramanadham@atyeti.com>
2025-06-25 18:07:01 +0530

    added append mode to the file_modes.py

--- python_learnings/File_Handling/file_modes.py
+++ python_learnings/File_Handling/file_modes.py
@@ -2,5 +2,16 @@ def read_file(file_name):
     with open(file_name,'r')as file:
         content=file.read()
     return content
-data=read_file('sample_file.txt')
-print(data)
+data1=read_file('sample_file.txt')
+print(data1)
+print("this is original data")
+def write_file(file_name):
+    with open(file_name,'a')as file:
+        file.write('Hy i have written added this line in to the original file, succesfully added this line')
+    return "added successfully"
+write_file('sample_file.txt')
+data2=read_file('sample_file.txt')
+print(data2)
+print("this is updated data")
+
+
