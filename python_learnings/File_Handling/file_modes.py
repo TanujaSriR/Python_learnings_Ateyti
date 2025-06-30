9d6b138449dfb19b33e0a2701d14f21a54305557
TanujaSriR <tanuja.ramanadham@atyeti.com>
2025-06-30 11:55:12 +0530

    Changed function name to improve readability

--- python_learnings/File_Handling/file_modes.py
+++ python_learnings/File_Handling/file_modes.py
@@ -5,11 +5,11 @@ def read_file(file_name):
 data1=read_file('sample_file.txt')
 print(data1)
 print("this is original data")
-def write_file(file_name):
+def append_file(file_name):
     with open(file_name,'a')as file:
         file.write('Hy i have written added this line in to the original file, succesfully added this line')
     return "added successfully"
-write_file('sample_file.txt')
+append_file('sample_file.txt')
 data2=read_file('sample_file.txt')
 print(data2)
 print("this is updated data")
